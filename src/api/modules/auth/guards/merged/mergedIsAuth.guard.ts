8b2125064e803ef7257e966f5e5c58bd2f746bdd
Ayomi <nikitakabo74@gmail.com>
2024-06-23 13:49:54 +0300

    guards fix

--- src/api/modules/auth/guards/merged/mergedIsAuth.guard.ts
+++ src/api/modules/auth/guards/merged/mergedIsAuth.guard.ts
@@ -1,5 +1,4 @@
 import { CanActivate, ExecutionContext, Inject } from '@nestjs/common';
-import { IsServerOwnerGuard } from '../isServerOwner.guard';
 import { IsBotGuard } from '../isBot.guard';
 import { IsAuthGuard } from '../isAuth.guard';
 import { JwtService } from '@nestjs/jwt';
--- src/api/modules/auth/guards/merged/mergedIsOwner.guard.ts
+++ src/api/modules/auth/guards/merged/mergedIsOwner.guard.ts
@@ -1,12 +1,21 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Inject } from '@nestjs/common';
 import { IsServerOwnerGuard } from '../isServerOwner.guard';
 import { IsBotGuard } from '../isBot.guard';
+import { IsAuthGuard } from '../isAuth.guard';
+import { JwtService } from '@nestjs/jwt';
 
 export class MergedIsOwner implements CanActivate {
+  constructor(@Inject(JwtService) private jwtService: JwtService) {}
   async canActivate(context: ExecutionContext) {
+    const isAuth = new IsAuthGuard(this.jwtService);
     const isOwner = new IsServerOwnerGuard();
     const isBot = new IsBotGuard();
-    const flags = await Promise.all([isOwner.canActivate(context), isBot]);
-    return flags.includes(true);
+    const isOwnerPromises = await Promise.all([
+      isAuth.canActivate(context),
+      isOwner.canActivate(context),
+    ]);
+    const flags = await Promise.all([isBot.canActivate(context)]);
+    if (isOwnerPromises.includes(false) || flags.includes(false)) return false;
+    return true;
   }
 }
